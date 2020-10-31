24f95ec1f9f888a14debe3db8b79ec60f8018a41
Trent Guillory <trentmichaelguillory@gmail.com>
2020-10-31 07:48:05 -0500

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Trent Guillory on 10/31/20.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
